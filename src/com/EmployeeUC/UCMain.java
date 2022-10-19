4b4a4a84c7027a5cdb053a75f5962cae61519ee9
SurabhiSanjan <surabhisanjan@gmail.com>
2022-10-19 18:48:27 +0530

    Modified: Daily Wage Calculator

--- src/com/EmployeeUC/UCMain.java
+++ src/com/EmployeeUC/UCMain.java
@@ -1,8 +1,20 @@
 package com.EmployeeUC;
-
+import java.util.Random;
 public class UCMain {
     public static void main(String args[]) {
-
+       int wage = 20;
+       int hour;
         System.out.println("Welcome to Employee Wage Program");
+        Random r = new Random();
+        int present = r.nextInt(2);
+        if (present == 0) {
+            System.out.println("Employee is Present");
+            hour = 8;
+        } else {
+            System.out.println("Employee is Absent");
+            hour=0;
+        }
+        int dailywage = wage*hour;
+        System.out.println("Daily wage is : "+ dailywage);
     }
 }
